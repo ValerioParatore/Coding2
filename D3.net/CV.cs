4baef4b0aa14cec7c36f3f4c7fec173118639bcf
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-08-30 14:40:16 +0200

    Esercizio mattina

---
+++ D3.net/CV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3.net
+{
+    public class CV
+    {
+        public InforPersonali infoPersonali { get; set; }
+        public List<Studi> studi { get; set; }    
+        public List<Impiego> impieghi { get; set; }    
+        
+    }
+    public class InforPersonali
+    {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public int tel { get; set; }
+        public string email { get; set; }
+    }
+    public class Studi
+    {
+        public string qualifica { get; set; }
+        public string istituto { get; set; }
+        public string tipo { get; set; }
+        public string data { get; set; }
+    }
+    public class Impiego
+    {
+        public Esperienza esperienza { get; set; }
+    }
+    public class Esperienza
+    {
+        public string azienda { get; set; }
+        public string jobTitle { get; set; }
+        public string data { get; set; }
+        public string descrizione { get; set; }
+        public string compiti { get; set; } 
+
+    }
+}
---
+++ D3.net/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CV cV = new CV();
+            InforPersonali infPersonali = new InforPersonali();
+
+            infPersonali.nome=
+            Esperienza esperienza = new Esperienza();
+            Esperienza esperienza1 = new Esperienza();
+            Esperienza esperienza2 = new Esperienza();
+            Studi studi = new Studi();
+            Studi studi1 = new Studi();
+            Studi studi2 = new Studi();
+            
+        }
+    }
+}
