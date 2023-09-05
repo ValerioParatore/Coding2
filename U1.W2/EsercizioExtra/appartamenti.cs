bf2aa70c67418713b20530a0f13dad9bd28941f0
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-09-05 15:05:02 +0200

    Esercizio extra

---
+++ U1.W2/EsercizioExtra/appartamenti.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EsercizioExtra
+{
+    public class Appartamenti
+    {
+        public int Vani { get; set; }
+        public bool terrazzoGiardino { get; set; }
+        public string TipoAppartamento { get; set; }
+        public int MetriQ { get; set; }
+        public string Zona { get; set; }
+        public Appartamenti() { }
+        public Appartamenti(int vani, bool giardinoOterrazza, string tipoAppartamento, int metriQ, string zona)
+        {
+            Vani = vani;
+            terrazzoGiardino = giardinoOterrazza;
+            TipoAppartamento = tipoAppartamento;
+            MetriQ = metriQ;
+            Zona = zona;
+
+        }
+
+
+
+    }
+
+}
