7a8d9d7ad16354303ac426c8ef847a35aab66fbc
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-09-06 16:10:57 +0200

    Aggiunta esercizio SQL

--- U1.W2/EsercizioExtraCC/Program.cs
+++ U1.W2/EsercizioExtraCC/Program.cs
@@ -15,3 +15,15 @@ namespace EsercizioExtraCC
         }
     }
 }
+//Esercizio SQL
+//select count(OrderID) as Ordini from Orders
+//select count(CustomerID) as clienti from Customers
+//select count(CustomerID) as clientiALondra from Customers where city = 'London'
+//select AVG(Freight) as costoTrasporti from Orders
+//select AVG(Freight) as costoTrasportiBOTTM from Orders where CustomerID = 'BOTTM'
+//select sum(Freight) as costoTrasportiPerID from Orders group by CustomerID
+//select count(CustomerID) as clientiPerCitta from Customers group by City
+//select sum(UnitPrice* Quantity) as quantitaXprezzo from[Order Details]  group by OrderID
+//select sum(UnitPrice * Quantity) as quantitaXprezzoID from[Order Details] where OrderID = 10248
+//select count(OrderID) as ordiniPerPaese from Orders group by ShipCity
+//select AVG(Freight) as costoMedioTrasporti from Orders group by ShipCity
